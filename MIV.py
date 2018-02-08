3c8867efa4b7329fbe6f1af3c7a9203ddb85ff1b
yuanjing <yuanjingjy@163.com>
2018-02-08 17:21:32 +0800

    平均影响值程序，直接画出图

--- MIV.py
+++ MIV.py
@@ -12,26 +12,27 @@ from imblearn.under_sampling import RandomUnderSampler
 from sklearn.model_selection import StratifiedKFold
 import matplotlib.pyplot as plt
 
-pandas_data = pd.read_csv('sql_eigen.csv')
+pandas_data = pd.read_csv('eigen62.csv')
 data = pandas_data.fillna(np.mean(pandas_data))
 
-data['age'][data['age'] > 200] = 91
-data2 = data.drop(['hr_cov', 'bpsys_cov', 'bpdia_cov', 'bpmean_cov', 'pulse_cov', 'resp_cov', 'spo2_cov','height'], axis=1)
-dataSet=np.array(data2)
-dataSet[:,0:78]=ann.preprocess(dataSet[:,0:78])
-dataSet[:,0:78]=ann.preprocess1(dataSet[:,0:78])
+data['age'][data['age'] > 200] = 91.4
+#data2 = data.drop(['hr_cov', 'bpsys_cov', 'bpdia_cov', 'bpmean_cov', 'pulse_cov', 'resp_cov', 'spo2_cov','height'], axis=1)
+data2=data
+dataSet=np.array(data2)#之前是程序删的无用项，现在手动删除
+dataSet[:,0:62]=ann.preprocess(dataSet[:,0:62])
+dataSet[:,0:62]=ann.preprocess1(dataSet[:,0:62])
 
-dataMat=dataSet[:,0:78]
-labelMat=dataSet[:,78]
+dataMat=dataSet[:,0:62]
+labelMat=dataSet[:,62]
 # dataMat=np.array(dataMat)
 # labelMat=np.array(labelMat)
 
-clf = MLPClassifier(hidden_layer_sizes=(78,), activation='tanh',
-                    shuffle=True, solver='sgd', alpha=1e-6, batch_size=5,
+clf = MLPClassifier(hidden_layer_sizes=(30,), activation='tanh',
+                    shuffle=True, solver='sgd', alpha=1e-6, batch_size=3,
                     learning_rate='adaptive')
 clf.fit(dataMat,labelMat)
 IV=[]
-for i in range(78):
+for i in range(62):
     tmpdata=dataMat.copy()
     tmpdata[:, i]=tmpdata[:,i]*0.8
     train_dec=tmpdata
@@ -50,12 +51,18 @@ print("test")
 
 datacolname= data2.drop(['class_label'], axis=1)#带名称的特征向量
 eigencounts = pd.DataFrame([IV],index=['score'],columns=datacolname.keys())
-sorteigen = eigencounts.sort_values(by='score',ascending=False, axis=1)
-
+sorteigen = eigencounts.sort_values(by='score',ascending=False, axis=1)#对MIV进行排序
+sorteigen.to_csv('D:/anntest.csv', encoding='utf-8', index=True)
 eigenwithname = pd.DataFrame(dataMat,columns=datacolname.keys())
+sorteigenname=sorteigen.keys()#MIV排序后的列名
+MIV30index=sorteigenname[0:30]
+MIV30=data2.loc[:,MIV30index]
+MIV30['class_label']=labelMat
+MIV30.to_csv('D:/MIV30.csv', encoding='utf-8', index=True)
+print()
 
 fitscore=[]
-for i in range(78):
+for i in range(62):
     col=sorteigen.keys()
     index=col[0:i+1]
     dataMat=eigenwithname.loc[:,index]
